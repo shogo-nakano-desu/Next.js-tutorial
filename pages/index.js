3bf172c4c26c5b4751c0fe1014e8c36cc476e95a
shogo-nakano-desu <61229807+shogo-nakano-desu@users.noreply.github.com>
2021-08-04 10:25:57 +0900

    change async func to arrow func

--- pages/index.js
+++ pages/index.js
@@ -3,14 +3,14 @@ import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
 import { getSortedPostsData } from "../lib/posts";
 
-export async function getStaticProps() {
+export const getStaticProps = async () => {
   const allPostData = getSortedPostsData();
   return {
     props: {
       allPostData,
     },
   };
-}
+};
 
 const Home = ({ allPostData }) => {
   return (
